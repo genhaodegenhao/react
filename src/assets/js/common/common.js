7e48e30e4bd53374f284f4c30701996f716377c9
song.wang.wb <song.wang.wb@external.99bill.com>
2017-08-10 12:40:59 +0800

    modi

--- src/assets/js/common/common.js
+++ src/assets/js/common/common.js
@@ -1,13 +1,5 @@
-/**
- * author:shimin.chen.wb;
- * time:2017/7/14;
- * description:公共js;
- */
-import $ from 'jquery';
-var isCheckAll = false;
 var utils = {
     baseUrl:'http://192.168.47.40:8109/app-rip-wd-movie-intra',//本地调试
-    //baseUrl:'https://192.168.8.124/rip-wd-movie-intra',
     // baseUrl:window.location.protocol+"//"+window.location.hostname+"/rip-wd-movie-intra",//线上
     formatCSTDate:function(strDate,format){
         return utils.formatDate(new Date(strDate),format);
@@ -37,22 +29,5 @@ var utils = {
         format || (format = "yyyy-MM-dd hh:mm:ss");
         return format.replace(/([a-z])(\1)*/ig,function(m){return cfg[m];});
     },
-    /**
-     *  全选和取消全选事件
-     */
-    toggleSelect: function(event){
-        //checkbox 全选/取消全选
-        if (isCheckAll) {
-            $("input[type='checkbox']").each(function() {
-                this.checked = false;
-            });
-            isCheckAll = false;
-        } else {
-            $("input[type='checkbox']").each(function() {
-                this.checked = true;
-            });
-            isCheckAll = true;
-        }
-    }
 };
 export default utils;
